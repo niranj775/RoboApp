add1dbc19610edaaad0106754a14801744294fd1
niranj775 <niranj775@gmail.com>
2020-09-27 16:46:22 +0530

    RoboApp

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import Searchbox from './Searchbox';
+import './App.css';
+
+class App extends Component{
+	constructor(){
+		super()
+		this.state={
+			robots: robots,
+			searchfield:'',
+		}
+	}
+
+	onSearchChange=(event)=>{
+		this.setState({searchfield: event.target.value})			}
+
+	render(){
+		const filteredRobots = this.state.robots.filter(robots=>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+
+	return (
+		<div className='tc'>
+			<h1 className='f2'>RoboFriends</h1>
+			<Searchbox searchChange={this.onSearchChange} />
+			<CardList robots={filteredRobots}/>
+		</div>
+		);
+}
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import Card from './Card';
+import App from './App';
+import 'tachyons';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />	, document.getElementById('root'));
+registerServiceWorker();
